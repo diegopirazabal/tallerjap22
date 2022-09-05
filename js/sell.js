f97da68897b1260923205d1fb7adc069f9eb3fce
Diego Pereyra <diego.pereyra007@gmail.com>
2022-09-04 23:02:07 -0300

    logout sell

--- js/sell.js
+++ js/sell.js
@@ -146,4 +146,12 @@ document.addEventListener("DOMContentLoaded", function(e){
             });
         }
     });
+
+    let user = JSON.parse(localStorage.getItem('usuarioLogeado'));
+    document.getElementById('usertag').innerHTML = user.nombre+' - Cerrar Sesion';
+    document.getElementById('usertag').addEventListener("click",()=>{
+        localStorage.removeItem('usuarioLogeado')
+        alert('Sesion Cerrada')
+        location.href = "index.html"
+       })
 });
