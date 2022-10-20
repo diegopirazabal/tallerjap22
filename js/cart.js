a3f006c93bc04770a9b17b48fdccc408f009be83
Diego Pereyra <diego.pereyra007@gmail.com>
2022-10-19 22:56:31 -0300

    fix

--- js/cart.js
+++ js/cart.js
@@ -1,5 +1,6 @@
 let usuario = 25801;
 let listaCarrito = [];
+
 function total(){
     let total = 0;
     let cantidad = parseInt((document.getElementById('unidades').value));
@@ -11,26 +12,37 @@ function total(){
     document.getElementById('subtotal').innerHTML = ` ${total}`;
 }
 
-function showCart(array){
+function showCart(){
         let HTMLContentToAppend = "";
 
 
-        for (let i = 0; i < listaCarrito.length; i++){
-            let item = listaCarrito[i];
+        // for (let i = 0; i < listaCarrito.length; i++){
+            let item = listaCarrito[0];
 
             HTMLContentToAppend += `
             <table class="table border-top">
             <tr>
-                <td scope="row"><img src="${item.image}" style="width: 80px;"></td>
-                <td><strong>${item.name}</strong></td>
-                <td><strong>${item.currency} <span>${item.unitCost}</strong></span></td>
-                <td><input type="number" class="form-control" id="unidades" placeholder="1" value="1" required></td>
-                <td><strong>${item.currency}<span id="subtotal"></span></strong></td>
+                <td scope="row">
+                    <img src="${item.image}" style="width: 80px;">
+                </td>
+                <td>
+                    <strong>${item.name}</strong>
+                </td>
+                <td>
+                    <strong>${item.currency} ${item.unitCost}</strong>
+                </td>
+                <td>
+                <input type="number" class="form-control" id="unidades" placeholder="1" value="1" required>
+                </td>
+                <td>
+                <strong>${item.currency}<span id="subtotal"></span></strong>
+                </td>
             </tr>
+            </table>
             
             `
             document.getElementById('carro').innerHTML = HTMLContentToAppend;
-        }
+        // }
     };
 
 
@@ -43,8 +55,8 @@ document.addEventListener("DOMContentLoaded", function(e){
     getJSONData(CART_INFO_URL + usuario + EXT_TYPE).then(function(resultObj){
         if (resultObj.status === "ok"){
             listaCarrito = resultObj.data.articles;
-            console.log(listaCarrito)
-            showCart(listaCarrito);
+            
+            showCart();
             total();
         }
     })
